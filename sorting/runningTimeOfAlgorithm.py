ba0c05c8bcceb41b3509c80324d58366770d828b
ZhaolianZhou <zhaolianzhou92@gmail.com>
2017-04-08 15:11:38 +1000

    insert sort time complexity

---
+++ sorting/runningTimeOfAlgorithm.py
@@ -0,0 +1,12 @@
+def insertion_sort(l):
+    shift = 0
+    for i in range(1, len(l)):
+        j = i-1
+        key = l[i]
+        while (j >= 0) and (l[j] > key):
+           l[j+1] = l[j]
+           shift+=1
+           j -= 1
+        l[j+1] = key
+
+    return shift
