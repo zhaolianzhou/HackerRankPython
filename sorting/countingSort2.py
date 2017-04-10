830a7461b4c4d6407981a6e957f8f2d13581649e
ZhaolianZhou <zhaolianzhou92@gmail.com>
2017-04-10 11:02:52 +1000

    count sort 2

---
+++ sorting/countingSort2.py
@@ -0,0 +1,14 @@
+def countSorting(N,A):
+    count = [0]*100
+    for i in range(N):
+        count[A[i]]+=1
+    for i in range(100):
+        Num_con = count[i]
+        for j in range(Num_con):
+            print(i,end=' ')
+
+# N = int(input().strip())
+# ar = [int(i) for i in input().strip().split()]
+ar = [1,2,3,4,3,6,7]
+N=7
+countSorting(N,ar)
